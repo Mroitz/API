0b461fc2090183f81041b97f3ba5fde2129e720e
morit <moritz.brandner@student.htldornbirn.at>
2025-01-30 10:40:23 +0100

    setup rest service

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require("express")
 
 const app = express();
 const port = 3000;
+
+const data = [
+    { name: "hans", age: 17 },
+    { name: "egon", age: 17 },
+    { name: "peter", age: 17 },
+    { name: "gurli", age: 17 },
+    { name: "hans", age: 17 },
+]
+
+app.get('/', (req, res) => {
+    res.send(data);
+});
+
 app.listen(port, () => {
     console.log("Server running on port " + port)
 })
