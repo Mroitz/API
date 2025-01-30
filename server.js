7999f6c71d3c9891b7ba203925396e414ffc5df3
morit <moritz.brandner@student.htldornbirn.at>
2025-01-30 11:13:41 +0100

    api done

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const bodyParser = require("body-parser")
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = 3000;
 
 const data = [
@@ -11,8 +14,26 @@ const data = [
     { name: "hans", age: 17 },
 ]
 
-app.get('/', (req, res) => {
+app.get('/people', (req, res) => {
     res.send(data);
+});
+
+app.get('/people/:id', (req, res) => {
+    let id = req.params.id;
+    console.log(req.params.id);
+
+    res.send(data[id]);
+});
+
+app.delete('/people/:id', (req, res) => {
+    let id = req.params.id;
+    data.splice(id, 1)
+    res.send("done");
+});
+
+app.post('/people', (req, res) => {
+    data.push(req.body);
+    res.send(req.body);
 });
 
 app.listen(port, () => {
